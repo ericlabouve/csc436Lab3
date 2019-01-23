a0619554d9ccaae7e93545470168c6b23ea517ce
Eric LaBouve <elabouve@csc235x06.ad.calpoly.edu>
2019-01-23 15:58:43 -0800

    saving picker

--- Lab3/ViewController.swift
+++ Lab3/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
             do {
                 let data = try Data(contentsOf:url)
                 let tempDict = try PropertyListSerialization.propertyList(from: data, options: [], format: nil) as! [String:[String]]
+                return tempDict
             } catch {
                 print(error)
             }
@@ -22,28 +23,47 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         return [:]
     }()
     
+    // Cities converted into an array so we can index dict later
+    var cities: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        cities = Array(dict.keys)
     }
-    
-    
-    
+
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 2
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        <#code#>
+        if component == 0 {
+            return dict.count
+        }
+        else if component == 1 {
+            // Get the string value from the current row in the first component
+            let cityIdx = pickerView.selectedRow(inComponent: 0)
+            let cityName = cities[cityIdx]
+            // Get the length the array pointed to by dict[key]
+            return (dict[cityName]?.count)!
+        }
+        return 0
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        <#code#>
+        if component == 0 {
+            return cities[row]
+        }
+        else if component == 1 {
+            let cityIdx = pickerView.selectedRow(inComponent: 0)
+            let cityName = cities[cityIdx]
+            return dict[cityName]![row]
+        }
+        return "!!!"
     }
 
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        <#code#>
-    }
+//    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+//        <#code#>
+//    }
 
 }
 
