788a6db753a01e157875a6f019cdeeb0631020b7
Eric LaBouve <elabouve@csc302x27.csc.calpoly.edu>
2019-01-23 10:21:39 -0800

    Initial Commit

---
+++ Lab3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lab3
+//
+//  Created by Eric LaBouve on 1/23/19.
+//  Copyright © 2019 Eric LaBouve. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
