2caac0697822510e3c2776f535667f6665d69c62
Eric LaBouve <elabouve@csc235x06.ad.calpoly.edu>
2019-01-23 20:21:11 -0800

    Finished Lab

--- Lab3/ViewController.swift
+++ Lab3/ViewController.swift
@@ -14,7 +14,11 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         if let url = Bundle.main.url(forResource:"Restaurants", withExtension:"plist") {
             do {
                 let data = try Data(contentsOf:url)
-                let tempDict = try PropertyListSerialization.propertyList(from: data, options: [], format: nil) as! [String:[String]]
+                var tempDict = try PropertyListSerialization.propertyList(from: data, options: [], format: nil) as! [String:[String]]
+                // Sort restaurants alphabetically
+                for (city, _) in tempDict {
+                    tempDict[city]!.sort()
+                }
                 return tempDict
             } catch {
                 print(error)
@@ -25,10 +29,15 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     
     // Cities converted into an array so we can index dict later
     var cities: [String] = []
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var pickerView: UIPickerView!
+    var curOrdering: String = "<"
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        cities = Array(dict.keys)
+    func setMessageLabel() {
+        let firstCompRow = pickerView.selectedRow(inComponent: 0)
+        let secondCompRow = pickerView.selectedRow(inComponent: 1)
+        let selectedCity = cities[firstCompRow]
+        messageLabel.text = selectedCity + " - " + dict[selectedCity]![secondCompRow]
     }
 
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -60,10 +69,43 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         }
         return "!!!"
     }
-
-//    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-//        <#code#>
-//    }
-
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        // Check if city changed
+        if component == 0 {
+            // Refresh the component with new restaurant labels
+            pickerView.reloadComponent(1)
+            // Set the selected row to the top
+            pickerView.selectRow(0, inComponent: 1, animated: false)
+        }
+        setMessageLabel()
+    }
+    
+    @IBAction func reverseOrdering(_ sender: UIButton) {
+        var operation: (String, String) -> Bool
+        if curOrdering == "<" {
+            operation = (>)
+            curOrdering = ">"
+        } else {
+            operation = (<)
+            curOrdering = "<"
+        }
+        cities.sort(by: operation)
+        for (city, _) in dict {
+            dict[city]!.sort(by: operation)
+        }
+        pickerView.selectRow(0, inComponent: 0, animated: false)
+        pickerView.selectRow(0, inComponent: 1, animated: false)
+        pickerView.reloadComponent(0)
+        pickerView.reloadComponent(1)
+        setMessageLabel()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        cities = Array(dict.keys)
+        cities.sort()
+        setMessageLabel()
+    }
 }
 
